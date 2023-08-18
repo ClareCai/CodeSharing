1eb6fa9d8ae0a4bf9caceb63cf87f7b24bb2c4f4
蔡偲 <caicai9514@163.com>
2023-08-18 17:17:57 +0800

    优化图标路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,20 +5,19 @@ import path from "path";
 
 const menus = fs
   .readdirSync(path.resolve(__dirname, "../"), { withFileTypes: true })
-  .filter(dirent => {
+  .filter((dirent) => {
     return /md/.test(dirent.name) && dirent.name !== "README.md";
   })
-  .map(dirent => dirent.name.replace(/.md$/, ""));
+  .map((dirent) => dirent.name.replace(/.md$/, ""));
+const base = "/CodeSharing/";
 
 export default defineUserConfig({
-  base: "/CodeSharing/",
+  base,
   dest: "./dist",
   lang: "zh-CN",
   title: "CaiCai 的分享库",
   description: "这是我的一个 代码分享 站点",
-  head: [
-    ["link", { rel: "icon", href: "/images/favicon.ico" }],
-  ],
+  head: [["link", { rel: "icon", href: base + "images/favicon.ico" }]],
   theme: defaultTheme({
     // 默认主题配置
     sidebar: menus,
