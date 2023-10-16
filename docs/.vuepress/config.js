08d4d05d3020244f99ffef7e0309f1c29cdedc59
蔡偲 <caicai9514@163.com>
2023-10-16 17:25:21 +0800

    优化config配置以及首页内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,28 @@
-import { defineUserConfig } from "vuepress";
-import { defaultTheme } from "vuepress";
-import fs from "fs";
-import path from "path";
+import { defineUserConfig } from 'vuepress';
+import { defaultTheme } from 'vuepress';
+import fs from 'fs';
+import path from 'path';
 
 const menus = fs
-  .readdirSync(path.resolve(__dirname, "../"), { withFileTypes: true })
-  .filter((dirent) => {
-    return /md/.test(dirent.name) && dirent.name !== "README.md";
-  })
-  .map((dirent) => dirent.name.replace(/.md$/, ""));
-const base = "/CodeSharing/";
+	.readdirSync(path.resolve(__dirname, '../'), { withFileTypes: true })
+	.filter((dirent) => {
+		return /md/.test(dirent.name) && dirent.name !== 'README.md';
+	})
+	.map((dirent) => dirent.name.replace(/.md$/, ''));
+const base = '/CodeSharing/';
 
 export default defineUserConfig({
-  base,
-  dest: "./dist",
-  lang: "zh-CN",
-  title: "CaiCai 的分享库",
-  description: "这是我的一个 代码分享 站点",
-  head: [["link", { rel: "icon", href: base + "images/favicon.ico" }]],
-  theme: defaultTheme({
-    // 默认主题配置
-    sidebar: menus,
-    sidebarDepth: 0,
-  }),
+	base,
+	dest: './dist',
+	lang: 'zh-CN',
+	title: 'CaiCai 的分享库',
+	description: '这是我的一个 代码分享 站点',
+	head: [['link', { rel: 'icon', href: base + 'images/favicon.ico' }]],
+	theme: defaultTheme({
+		// 默认主题配置
+		sidebar: menus,
+		sidebarDepth: 0,
+		repo: 'https://github.com/ClareCai/CodeSharing',
+		editLink: false,
+	}),
 });
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,9 @@
-# CaiCai 的代码分享库
+---
+home: true
+heroText: CaiCai 的代码分享库
+tagline: 这是我的一个 代码分享 站点
+actions:
+  - text: 阅读
+    link: /Table组件列宽度计算优化.md
+    type: primary
+---
