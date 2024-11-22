7bb2f9fc00bc465ca48af5fac22925e3834c3e0b
ClareCai <caicai9514@163.com>
2024-11-22 16:57:55 +0800

    底部增加备案信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,15 @@
-import { defineUserConfig } from 'vuepress';
-import { defaultTheme } from 'vuepress';
-import fs from 'fs';
-import path from 'path';
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
+import fs from 'fs'
+import path from 'path'
 
 const menus = fs
 	.readdirSync(path.resolve(__dirname, '../'), { withFileTypes: true })
-	.filter((dirent) => {
-		return /md/.test(dirent.name) && dirent.name !== 'README.md';
+	.filter(dirent => {
+		return /md/.test(dirent.name) && dirent.name !== 'README.md'
 	})
-	.map((dirent) => dirent.name.replace(/.md$/, ''));
-const base = '/CodeSharing/';
+	.map(dirent => dirent.name.replace(/.md$/, ''))
+const base = '/CodeSharing/'
 
 export default defineUserConfig({
 	base,
@@ -17,12 +17,15 @@ export default defineUserConfig({
 	lang: 'zh-CN',
 	title: 'CaiCai 的分享库',
 	description: '这是我的一个 代码分享 站点',
-	head: [['link', { rel: 'icon', href: base + 'images/favicon.ico' }]],
+	head: [
+		['link', { rel: 'icon', href: base + 'images/favicon.ico' }],
+		['link', { rel: 'stylesheet', href: base + 'css/custom.css' }]
+	],
 	theme: defaultTheme({
 		// 默认主题配置
 		sidebar: menus,
 		sidebarDepth: 0,
 		repo: 'https://github.com/ClareCai/CodeSharing',
-		editLink: false,
-	}),
-});
+		editLink: false
+	})
+})
--- docs/README.md
+++ docs/README.md
@@ -6,4 +6,12 @@ actions:
   - text: 阅读
     link: /Table组件列宽度计算优化.md
     type: primary
+footer: 
+  Copyright © All rights reserved 
+   | 
+  <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2024025699号</a>
+   | 
+  <img src="./images/registration-logo.png" class="beian-img" />
+  <a href="https://beian.mps.gov.cn/#/query/webSearch?code=12010202000886" rel="noreferrer" target="_blank">津公网安备12010202000886</a>
+footerHtml: true
 ---
